f3a17bb492507c59e697caf6fe126eb713177f90
ItsBlackGear <68313143+ItsBlackGear@users.noreply.github.com>
2025-06-03 01:53:24 -0400

    💧 Somehow, i forgot to make resin clumps waterloggable...
    - Made resin clumps waterloggable
    - Added NoOpSpreadConfig to prevent resin clump to spread... just in case 🤷‍♀️
    - Renamed Summer Drop creative tab to "Chase The Skies"

--- common/src/main/java/com/blackgear/vanillabackport/common/level/blocks/ResinClumpBlock.java
+++ common/src/main/java/com/blackgear/vanillabackport/common/level/blocks/ResinClumpBlock.java
@@ -1,15 +1,65 @@
 package com.blackgear.vanillabackport.common.level.blocks;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.MultifaceBlock;
 import net.minecraft.world.level.block.MultifaceSpreader;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.jetbrains.annotations.Nullable;
 
-public class ResinClumpBlock extends MultifaceBlock {
+public class ResinClumpBlock extends MultifaceBlock implements SimpleWaterloggedBlock {
+    private static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
     public ResinClumpBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+
+        return super.updateShape(state, direction, neighborState, level, pos, neighborPos);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED)
+            ? Fluids.WATER.getSource(false)
+            : super.getFluidState(state);
     }
 
     @Override
     public MultifaceSpreader getSpreader() {
-        return null;
+        return new MultifaceSpreader(new NoOpSpreadConfig());
+    }
+
+    private static class NoOpSpreadConfig implements MultifaceSpreader.SpreadConfig {
+        @Override
+        public @Nullable BlockState getStateForPlacement(BlockState currentState, BlockGetter level, BlockPos pos, Direction lookingDirection) {
+            return null;
+        }
+
+        @Override
+        public boolean canSpreadInto(BlockGetter level, BlockPos pos, MultifaceSpreader.SpreadPos spreadPos) {
+            return false;
+        }
     }
 }
