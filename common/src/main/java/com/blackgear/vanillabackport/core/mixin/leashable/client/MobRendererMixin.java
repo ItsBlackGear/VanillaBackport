aaac3e24e9b8c75a7b44cd57ccd55d563cdc83d8
ItsBlackGear <68313143+ItsBlackGear@users.noreply.github.com>
2025-06-15 19:19:28 -0400

    📦 Simplify MobRenderer

--- common/src/main/java/com/blackgear/vanillabackport/core/mixin/leashable/client/MobRendererMixin.java
+++ common/src/main/java/com/blackgear/vanillabackport/core/mixin/leashable/client/MobRendererMixin.java
@@ -1,50 +1,19 @@
 package com.blackgear.vanillabackport.core.mixin.leashable.client;
 
-import com.blackgear.vanillabackport.common.api.leash.LeashRenderer;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.culling.Frustum;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobRenderer.class)
-public abstract class MobRendererMixin<T extends Mob, M extends EntityModel<T>> extends LivingEntityRenderer<T, M> {
-    @Unique private LeashRenderer<T> leashRenderer;
-
-    public MobRendererMixin(EntityRendererProvider.Context context, M model, float shadowRadius) {
-        super(context, model, shadowRadius);
-    }
-
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void vb$init(EntityRendererProvider.Context context, M model, float shadowRadius, CallbackInfo ci) {
-        this.leashRenderer = new LeashRenderer<>(this.entityRenderDispatcher);
-    }
-
-    @Inject(
-        method = "render(Lnet/minecraft/world/entity/Mob;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/renderer/entity/MobRenderer;renderLeash(Lnet/minecraft/world/entity/Mob;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/world/entity/Entity;)V"
-        ),
-        cancellable = true
-    )
-    private void onRenderLeash(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
-        this.leashRenderer.render(entity, partialTicks, poseStack, buffer);
+public abstract class MobRendererMixin<T extends Mob> extends EntityRendererMixin<T> {
+    @Inject(method = "renderLeash", at = @At("HEAD"), cancellable = true)
+    private <E extends Entity> void onRenderLeash(T entity, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, E leashHolder, CallbackInfo ci) {
         ci.cancel();
-    }
-
-    @Inject(method = "shouldRender(Lnet/minecraft/world/entity/Mob;Lnet/minecraft/client/renderer/culling/Frustum;DDD)Z", at = @At("HEAD"), cancellable = true)
-    private void shouldRenderLeash(T entity, Frustum camera, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
-        boolean renderLeash = this.leashRenderer.shouldRender(entity, camera, super.shouldRender(entity, camera, camX, camY, camZ), false);
-        if (renderLeash) cir.setReturnValue(true);
     }
 }
