3fabdd5dbbd13cb6e5b951734cc6521da26e3a2f
ItsBlackGear <68313143+ItsBlackGear@users.noreply.github.com>
2025-06-14 22:33:27 -0400

    💿 Tears Record + sniffer leashable

--- common/src/main/java/com/blackgear/vanillabackport/core/mixin/leashable/common/entities/LeashablesMixin.java
+++ common/src/main/java/com/blackgear/vanillabackport/core/mixin/leashable/common/entities/LeashablesMixin.java
@@ -2,7 +2,9 @@ package com.blackgear.vanillabackport.core.mixin.leashable.common.entities;
 
 import com.blackgear.vanillabackport.common.api.leash.Leashable;
 import net.minecraft.world.entity.animal.camel.Camel;
+import net.minecraft.world.entity.animal.horse.AbstractChestedHorse;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.animal.sniffer.Sniffer;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -25,6 +27,32 @@ public class LeashablesMixin {
         @Override
         public Vec3[] getQuadLeashOffsets() {
             return Leashable.createQuadLeashOffsets((AbstractHorse)(Object)this, 0.04, 0.52, 0.23, 0.87);
+        }
+    }
+
+    @Mixin(AbstractChestedHorse.class)
+    public static class AbstractChestedHorseMixin implements Leashable {
+        @Override
+        public boolean supportQuadLeash() {
+            return true;
+        }
+
+        @Override
+        public Vec3[] getQuadLeashOffsets() {
+            return Leashable.createQuadLeashOffsets((AbstractChestedHorse)(Object)this, 0.04, 0.41, 0.18, 0.73);
+        }
+    }
+
+    @Mixin(Sniffer.class)
+    public static class SnifferMixin implements Leashable {
+        @Override
+        public boolean supportQuadLeash() {
+            return true;
+        }
+
+        @Override
+        public Vec3[] getQuadLeashOffsets() {
+            return Leashable.createQuadLeashOffsets((Sniffer)(Object)this, -0.01, 0.63, 0.38, 1.15);
         }
     }
 }
--- fabric/src/main/java/com/blackgear/vanillabackport/data/server/tags/ItemTagGenerator.java
+++ fabric/src/main/java/com/blackgear/vanillabackport/data/server/tags/ItemTagGenerator.java
@@ -53,6 +53,8 @@ public class ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
         this.getOrCreateTagBuilder(ModItemTags.HAPPY_GHAST_TEMPT_ITEMS)
             .addTag(ModItemTags.HAPPY_GHAST_FOOD)
             .addTag(ModItemTags.HARNESSES);
+
+        this.getOrCreateTagBuilder(ItemTags.MUSIC_DISCS).add(ModItems.MUSIC_DISC_TEARS.get());
     }
 
     private void addWoodRelatedTags() {
