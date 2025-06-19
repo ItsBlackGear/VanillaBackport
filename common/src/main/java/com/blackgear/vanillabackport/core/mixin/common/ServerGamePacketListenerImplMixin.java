9f38abc6ecf88a1a5ed49cd16d7a3023b9462c91
ItsBlackGear <68313143+ItsBlackGear@users.noreply.github.com>
2025-06-19 19:26:11 -0400

    🚀 1.2.4 - Release Candidate
    - servers no longer kick players for flying ghasts ("flying is not enabled on the server")
    - added saddle recipe
    - updated lead recipe
    - version bump

---
+++ common/src/main/java/com/blackgear/vanillabackport/core/mixin/common/ServerGamePacketListenerImplMixin.java
@@ -0,0 +1,26 @@
+package com.blackgear.vanillabackport.core.mixin.common;
+
+import com.blackgear.vanillabackport.common.level.entities.happyghast.HappyGhast;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public class ServerGamePacketListenerImplMixin {
+    @Shadow public ServerPlayer player;
+
+    @Shadow private int aboveGroundVehicleTickCount;
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void vb$preventFlyingKick(CallbackInfo ci) {
+        Entity vehicle = this.player.getVehicle();
+        if (vehicle instanceof HappyGhast ghast && !ghast.isBaby()) {
+            this.aboveGroundVehicleTickCount = 0;
+        }
+    }
+}
