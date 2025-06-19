729745a945ea4c26e520e35ade810b2b0afae18f
ItsBlackGear <68313143+ItsBlackGear@users.noreply.github.com>
2025-06-19 19:13:24 -0400

    🧵 Leash Rendering

---
+++ common/src/main/java/com/blackgear/vanillabackport/core/mixin/leash/EntityRendererMixin.java
@@ -0,0 +1,42 @@
+package com.blackgear.vanillabackport.core.mixin.leash;
+
+import com.blackgear.vanillabackport.common.api.leash.LeashRenderer;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Leashable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityRenderer.class)
+public class EntityRendererMixin<T extends Entity> {
+    @Unique private LeashRenderer<T> leashRenderer;
+
+    @Shadow @Final protected EntityRenderDispatcher entityRenderDispatcher;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void vb$init(EntityRendererProvider.Context context, CallbackInfo ci) {
+        this.leashRenderer = new LeashRenderer<>(this.entityRenderDispatcher);
+    }
+
+    @Inject(method = "renderLeash", at = @At("HEAD"), cancellable = true)
+    private <E extends Entity & Leashable> void vb$renderLeash(T entity, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, E leashHolder, CallbackInfo ci) {
+        ci.cancel();
+        this.leashRenderer.render(entity, partialTick, poseStack, bufferSource);
+    }
+
+    @Inject(method = "shouldRender", at = @At("TAIL"), cancellable = true)
+    private void vb$shouldRender(T entity, Frustum camera, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(this.leashRenderer.shouldRender(entity, camera, cir.getReturnValue()));
+    }
+}
